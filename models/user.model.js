0dd8b1082487c4530ba00036c540b42423355724
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-10-27 12:50:12 +0300

    used bcryptjs to hash passwords and used mongoose middlewares to check for email uniquness

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -42,6 +43,16 @@ userSchema.statics.isEmailTaken = async function (email) {
   const user = await this.findOne({ email });
   return !!user;
 };
+userSchema.methods.isPasswordMatch = async function (password) {
+  const user = this;
+  return await bcrypt.compare(password, user.password);
+};
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password"))
+    user.password = await bcrypt.hash(user.password, 8);
+  next();
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const validate = require('./../middlewares/validate');
-const { userValidation } = require('./../validations');
-const { authController } = require('./../controllers');
+const validate = require("./../middlewares/validate");
+const { userValidation, authValidation } = require("./../validations");
+const { authController } = require("./../controllers");
 router.post(
-  '/auth/register',
+  "/auth/register",
   validate(userValidation.createUserSchema),
   authController.register
+);
+router.post(
+  "/auth/login",
+  validate(authValidation.loginSchema),
+  authController.login
 );
 
 module.exports = router;
